e54b9a66bcb07f52387691dffbe95e2c824476f6
Gabrieli <gabrielidelimamartins@gmail.com>
2025-12-17 10:10:40 -0300

    feat: criação do metodo findall

---
+++ src/categoria/controllers/categoria.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { CategoriaService } from '../services/categoria.service';
+import { Categoria } from '../entities/categoria.entity';
+
+@Controller('/categorias')
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
+}
