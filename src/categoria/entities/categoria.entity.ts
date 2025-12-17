dbd7ab7558903816a8a630c5cd0b3e719660361c
Gabrieli <gabrielidelimamartins@gmail.com>
2025-12-17 09:57:07 -0300

    feat: criação da entity

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,28 @@
+import { IsNotEmpty } from 'class-validator';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  titulo: string;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  descricao: string;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  setor: string;
+
+  @UpdateDateColumn()
+  dataChegada: Date;
+}
